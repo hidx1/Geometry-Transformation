deaa8adce6105873b6bc5510a4385d506c34bf70
Nixon Andhika <43450143+hidx1@users.noreply.github.com>
2018-11-10 18:41:11 +0700

    Update calculation.py

--- calculation.py
+++ calculation.py
@@ -1,29 +1,47 @@
 import numpy
 import copy
 
+def ConvertTo2D(matrix):
+    matrix_result = numpy.zeros((len(matrix[:,0]), 2))
+    for i in range(len(matrix[:,0])):
+        for j in range(2):
+            matrix_result[i][j] = matrix[i][j]
+    return matrix_result
+
 def translate(matrix, dx, dy, dz):
-    #procedure to translate matrix
-    transform = numpy.identity(4)
-    transform[3,0] = dx
-    transform[3,1] = dy
-    transform[3,2] = dz
-    matrix = numpy.mat(matrix) * numpy.mat(transform)
+    for i in range(len(matrix[:,0])):
+        matrix[i][0] += dx
+        matrix[i][1] += dy
+        matrix[i][2] += dz
     return matrix
 
 def dilate(matrix, k):
-    #procedure to dilate matrix
-    transform = numpy.identity(4)
-    for i in range(3):
-        transform[i,i] = k
-    transform[3,3] = 1
-    matrix = numpy.mat(matrix) * numpy.mat(transform)
+    for i in range(len(matrix[:,0])):
+        for j in range(3):
+            matrix[i][j] *= k
     return matrix
 
-def rotate(matrix, deg, a, b):
+def rotate(matrix, dim, deg, a, b):
     print("rotate")
 
-def reflect(matrix, param):
-    print("reflect")
+def reflect(matrix, dim, param):
+    if dim == 2:
+        if param == "x":
+            transform = numpy.array([[1,0],
+                                      [0,-1]])
+        elif param == "y":
+            transform = numpy.array([[-1,0],
+                                      [0,1]])
+        elif param == "y=x":
+            transform = numpy.array([[0,1],
+                                      [1,0]])
+        elif param == "y=-x":
+            transform = numpy.array([[0,-1],
+                                      [-1,0]])
+        elif param == "(a,b)":
+            print("dsaxcz")
+    elif dim == 3:
+        print("sadas")
 
 def shear(matrix, param, k):
     not_error = True
@@ -42,23 +60,67 @@ def shear(matrix, param, k):
         not_error = False
 
     if not_error:
-        transform[1,0], transform[2,0] = x
-        transform[0,1], transforn[2,1] = y
-        transform[0,2], transform[1,2] = z
-    return matrix
+        transform[1,0] = x
+        transform[2,0] = x
+        transform[0,1] = y
+        transform[2,1] = y
+        transform[0,2] = z
+        transform[1,2] = z
+    for i in range(len(matrix[:,0])):
+        matrix[i,:] = numpy.matmul(matrix[i,:], transform)
 
-def stretch(matrix, param, k):
-    print("stretch")
-    return matrix
+def stretch(matrix, dim, param, k):
+    not_error = True
+    x = 1
+    y = 1
+    z = 1
 
-def custom(matrix, a, b, c, d):
-    print("custom")
-    return matrix
+    if param == "x":
+        x = k
+    elif param == "y":
+        y = k
+    elif param == "z":
+        z = k
+    else:
+        print("Error, parameter salah")
+        not_error = False
 
-def multiple(matrix, n):
+    if not_error:
+        if dim == 2:
+            #transform = numpy.array([[x,0,0],
+            #                        [0,y,0],
+            #                        [0,0,1]])
+            transform = numpy.array([[1,k],
+                                     [0,1]])
+            matrix_temp = ConvertTo2D(matrix)
+            matrix_temp = numpy.matmul(matrix_temp, transform)
+            for i in range(len(matrix[:,0])):
+                for j in range(2):
+                    matrix[i][j] = matrix_temp[i][j]
+            return matrix
+        #return numpy.matmul(matrix, transform)
+
+def custom(matrix, dim, a, b, c, d, e, f, g, h, i):
+    if dim == 2:
+        #Linear transformation for 2D
+        matrix_temp = ConvertTo2D(matrix) #Create temporary N x 2 matrix
+        #Create 2 x 2 transformation matrix
+        transform = numpy.array([[a,b],
+                                  [c,d]])
+        #Matrix multiplication between matrix_temp and transform
+        matrix_temp = numpy.matmul(matrix_temp, transform)
+        #Copy x and y element from N x 2 matrix to N x 4 matrix
+        for i in range(len(matrix[:,0])):
+            for j in range(2):
+                matrix[i][j] = matrix_temp[i][j]
+        return matrix
+    else:
+        #Linear transformation for 3D
+        #Create 3 x 3 transformation matrix
+        transform = numpy.array([[a,b,c],
+                                  [d,e,f],
+                                  [g,h,i]])
+        return numpy.matmul(matrix, transform)
+
+def multiple(matrix, dim, n):
     print("multiple")
-    return matrix
-
-def reset(matrix_original, matrix_calculation):
-    matrix_calculation = copy.deepcopy(matrix.original)
-    return matrix
