74cb62f3f8b64f6353355b6c6681c96b4ee9dbf1
yoelpro <yoelsusantopro@yahoo.com>
2018-11-11 10:40:31 +0700

    Create new file for draw functions

--- main.py
+++ main.py
@@ -9,106 +9,12 @@ import time
 import queue
 from calculation import *
 from pygame.locals import *
+from draw import *
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
 q = queue.Queue()
 numpy.set_printoptions(suppress=True) #prevent scientific notation
-
-colors = (
-    (1,0,0),
-    (0,1,0),
-    (0,0,1),
-    (0,1,0),
-    (1,1,1),
-    (0,1,1),
-    (1,0,0),
-    (0,1,0),
-    (0,0,1),
-    (1,0,0),
-    (1,1,1),
-    (0,1,1),
-    )
-
-verticies = (
-    (1, -1, -1),
-    (1, 1, -1),
-    (-1, 1, -1),
-    (-1, -1, -1),
-    (1, -1, 1),
-    (1, 1, 1),
-    (-1, -1, 1),
-    (-1, 1, 1)
-    )
-
-edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7)
-    )
-
-surfaces = (
-    (0,1,2,3),
-    (3,2,7,6),
-    (6,7,5,4),
-    (4,5,1,0),
-    (1,5,7,2),
-    (4,0,3,6)
-    )
-
-def Cube(): #Draw cube
-    glBegin(GL_QUADS)
-    for surface in surfaces:
-        x = 0
-        for vertex in surface:
-            x+=1
-            glColor3fv(colors[x])
-            glVertex3fv(verticies[vertex])
-    glEnd()
-
-    glBegin(GL_LINES)
-    for edge in edges:
-        for vertex in edge:
-            glVertex3fv(verticies[vertex])
-    glEnd()
-
-def Axis(): #Draw x, y, z axes
-    glBegin(GL_LINES)
-    glColor3f(1, 0, 0)
-    glVertex3f(-50, 0, 0)
-    glVertex3f(50, 0, 0)
-
-    glColor3f(0, 1, 0)
-    glVertex3f(0, -50, 0)
-    glVertex3f(0, 50, 0)
-
-    glColor3f(0, 0, 1)
-    glVertex3f(0, 0, -50)
-    glVertex3f(0, 0, 50)
-    glEnd()
-
-def Draw2D(matrix):
-    glBegin(GL_LINES)
-    glColor3f(1, 1, 1)
-    for i in range(0,len(matrix)):
-        #print(matrix[i % len(matrix)])
-        glVertex3fv(matrix[i % len(matrix)])
-        glVertex3fv(matrix[(i+1) % len(matrix)])
-    glEnd()
-    glBegin(GL_POLYGON)
-    glColor3f(0, 1, 0)
-    for i in range(0,len(matrix)):
-        glVertex3fv(matrix[i % len(matrix)])
-    glEnd()
 
 def IdentityMat44():
     return numpy.matrix(numpy.identity(4), copy=False, dtype='float32')
